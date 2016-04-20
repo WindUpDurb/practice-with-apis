93dc8a3673c80a49a4ce6b3ec654e4b801e7f0a9
David Urbina <durbina1991@gmail.com>
2016-04-19 21:55:35 -0700

    Ajax Working For Gravatar

--- public/main.js
+++ public/main.js
@@ -2,10 +2,27 @@
  * Created by david on 4/19/16.
  */
 
+var operations = {
+
+    gravatarSearchClick : function () {
+        $("#gravatarSearch").click(function () {
+            var toSearch = $("#searchGravatar").val();
+            $.get(`/gravatar/${toSearch}`, function (data) {
+                var $toAppend = $("<img>").attr("src", data);
+                console.log(data)
+                $("#gravatarDiv").append($toAppend);
+            })
+        })
+    }
 
 
-$(document).ready(function () {
+}
 
-    
 
-})
+
+var initialize = function () {
+    operations.gravatarSearchClick();
+};
+
+
+$(document).ready(initialize);
